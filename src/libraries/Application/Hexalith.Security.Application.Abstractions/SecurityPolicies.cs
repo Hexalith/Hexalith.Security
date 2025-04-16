91e2da59ff273e396d64028e86f143555f617aa9
Jérôme Piquot <jpiquot@itaneo.com>
2025-04-16 12:20:24 +0200

    feat: add new projects and security management classes
    - Updated `Hexalith.Security.sln` to include new projects: `.tools`, `Hexalith.Security.Application`, and `Hexalith.Security.Application.Abstractions`.
    - Created `Hexalith.Security.Application.Abstractions.csproj` with target framework `net9.0` and necessary package references.
    - Introduced `SecurityPolicies` and `SecurityRoles` classes to manage access control within the application.
    - Added `flush_redis.bat` for flushing Redis data using Docker.
    - Noted duplicate changes in the diff, likely due to an error in version control.

---
+++ src/libraries/Application/Hexalith.Security.Application.Abstractions/SecurityPolicies.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SecurityPolicies.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Security.Application;
+
+/// <summary>
+/// Defines the policies for security module within the application.
+/// </summary>
+public static class SecurityPolicies
+{
+    /// <summary>
+    /// Policy for users who can contribute to documents.
+    /// </summary>
+    public const string Contributors = nameof(Security) + nameof(Contributors);
+
+    /// <summary>
+    /// Policy for users who own documents.
+    /// </summary>
+    public const string Owners = nameof(Security) + nameof(Owners);
+
+    /// <summary>
+    /// Policy for users who can read documents.
+    /// </summary>
+    public const string Readers = nameof(Security) + nameof(Readers);
+}
---
+++ src/libraries/Application/Hexalith.Security.Application.Abstractions/SecurityRoles.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SecurityRoles.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Hexalith.Security.Application;
+
+/// <summary>
+/// Defines the roles for document security within the application.
+/// </summary>
+public static class SecurityRoles
+{
+    /// <summary>
+    /// Role for users who can contribute to documents.
+    /// </summary>
+    public const string Contributor = nameof(Security) + nameof(Contributor);
+
+    /// <summary>
+    /// Role for users who own documents.
+    /// </summary>
+    public const string Owner = nameof(Security) + nameof(Owner);
+
+    /// <summary>
+    /// Role for users who can read documents.
+    /// </summary>
+    public const string Reader = nameof(Security) + nameof(Reader);
+}
