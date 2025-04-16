dcb6ad0357afbc41a2481011bed1b97540a36f0a
Jérôme Piquot <jpiquot@itaneo.com>
2025-04-16 09:23:01 +0200

    chore: add copyright and update Program.cs
    - Added copyright information to the top of `Program.cs`.
    - Changed `builder` variable declaration to use `IDistributedApplicationBuilder`.
    - Updated application run method to `RunAsync` for asynchronous execution.

--- AspireHost/Program.cs
+++ AspireHost/Program.cs
@@ -1,3 +1,8 @@
-var builder = DistributedApplication.CreateBuilder(args);
+// <copyright file="Program.cs" company="ITANEO">
+// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
 
-builder.Build().Run();
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+await builder.Build().RunAsync();
